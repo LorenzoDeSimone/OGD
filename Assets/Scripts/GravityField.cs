67c20073f2c2b8b53748d5e0142be4e96cda0242
Lorenzo De Simone <lorenz.desimone@gmail.com>
2017-03-19 16:12:24 +0100

    New integration of gravity field and movement

---
+++ Assets/Scripts/GravityField.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityField : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        //if (coll.gameObject.tag.StartsWith("Robot"))
+        //{
+
+        //}
+
+        PlayerController player = collider.GetComponent<PlayerController>();
+        player.setGravityCenter(this);
+        Debug.Log(transform.position);
+
+        //collider.GetComponent<ComponentType>();
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpSpeed;
+    public float jumpRate;
+
+    private Rigidbody2D rb;
+    private bool jumpAble;
+    private float nextJump;
+    private Transform childGuide;
+    public GravityField myGravityField;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jumpAble = true;
+        nextJump = Time.time;
+        childGuide = transform.FindChild("guide");
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVerical = Input.GetAxis("Vertical");
+        float playerAngle = transform.eulerAngles.z * Mathf.Deg2Rad;
+        float sinPlayerAngle = Mathf.Sin(playerAngle);
+        float cosPlayerAngle = Mathf.Cos(playerAngle);
+        float joystickAngle = Mathf.Acos(moveHorizontal);
+        if (moveVerical < 0)
+            joystickAngle = -joystickAngle;
+        if (moveHorizontal != 0 || moveVerical != 0)
+        {
+            float movement = Mathf.Cos(joystickAngle - playerAngle) * Mathf.Sqrt(moveHorizontal * moveHorizontal + moveVerical * moveVerical);
+            transform.position += new Vector3(movement * cosPlayerAngle, movement * sinPlayerAngle, 0) * speed * Time.fixedDeltaTime;
+            childGuide.position = transform.position + new Vector3(moveHorizontal, moveVerical, 0);
+        }
+        else
+        {
+            childGuide.position = transform.position;
+        }
+        if (Input.GetKeyDown(KeyCode.JoystickButton0) == true && Time.time > nextJump && Physics2D.Raycast(new Vector2(transform.position.x - 2 * sinPlayerAngle, transform.position.y - 2 * cosPlayerAngle), new Vector2(-sinPlayerAngle, -cosPlayerAngle), 2))
+        {
+            rb.velocity = new Vector2(-jumpSpeed * sinPlayerAngle, jumpSpeed * cosPlayerAngle);
+            nextJump = Time.time + jumpRate;
+        }
+
+
+
+        RaycastHit2D myGround = Physics2D.Raycast(transform.position, myGravityField.transform.position - transform.position, Mathf.Infinity, LayerMask.GetMask("Walkable"));
+        //Debug.Log(myGravityField.transform.position);
+        //Debug.Log("Mypos"+transform.position);
+        //Vector2 myUnderPos = transform.position + Vector3.down * 2;
+        //Debug.Log(-myGround.normal);
+
+
+        //Debug.DrawLine(transform.position, myGravityField.transform.position);
+        //Debug.Log(myGround.point);
+        //Debug.DrawLine(transform.position, myGround.point);
+
+        //        GetComponent<Rigidbody2D>().AddForce(Vector2.down * 100);
+        GetComponent<Rigidbody2D>().AddForce(-myGround.normal * 100);
+        //transform.up = Vector2.Lerp(transform.up, transform.position-myGravityField.transform.position, Time.deltaTime*10);
+        transform.up = Vector2.Lerp(transform.up, myGround.normal, Time.deltaTime * 10);
+    }
+
+    public void setGravityCenter(GravityField newGravityField)
+    {
+        myGravityField = newGravityField;
+    }
+}
