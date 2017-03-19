883c7a397fa3717af0d769c5d7abd8cc5140beb5
Andrea Jegher <andreaj-93@hotmail.it>
2017-03-19 17:41:55 +0100

    Aggiunte le scene di test per il network

---
+++ Assets/Scripts/Networking/NetworkLobbyController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+namespace Networking
+{
+	public class NetworkLobbyController : NetworkLobbyManager {
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+	}
+}
