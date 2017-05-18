0161188652588c1e5ac3679726a7e82e1a050ed6
Emanuele <emanuele.lattanzio@mail.polimi.it>
2017-05-18 19:17:58 +0200

    first version of correct spawn

--- Assets/Scripts/Spawn collectable/CollectableSpawner.cs
+++ Assets/Scripts/Spawn collectable/CollectableSpawner.cs
@@ -1,36 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.Spawn_collectable
 {
     class CollectableSpawner : NetworkBehaviour
     {
         public GameObject collectablePrefab;
-        public float spawnTime = 5.0f;
-        public float bias = 0.0f;
+        public GameObject countdown;
 
-        private List<Transform> spawnPositions;
+        public int minCountdown = 10;
+        public int maxCountdown = 20;
+
         private HashSet<GameObject> collectables;
+        private CountDown countdownCounter;
 
         private void Start()
         {
             collectables = new HashSet<GameObject>();
-            spawnPositions = new List<Transform>(GetComponentsInChildren<Transform>());
-            //remove my tranform
-            spawnPositions.Remove(transform);
-            InitCollectables();
-            InvokeRepeating("SpawnCoins", 2.0f, spawnTime);
+            GameObject go = Instantiate(countdown, transform.position, Quaternion.identity, transform);
+            NetworkServer.Spawn(go);
+            countdownCounter = go.GetComponent<CountDown>();
+            StartCoroutine(firstCountdown());
         }
 
         private void InitCollectables()
         {
             GameObject go;
-            foreach( Transform t in spawnPositions )
+            foreach ( Transform t in transform )
             {
-                go = Instantiate(collectablePrefab, t.position, Quaternion.identity, transform);
-                collectables.Add(go);
-                NetworkServer.Spawn(go);
+                if(t.gameObject.tag == "SpawnPoint")
+                {
+                    go = Instantiate(collectablePrefab, t.position, Quaternion.identity, transform);
+                    collectables.Add(go);
+                    NetworkServer.Spawn(go);
+                }
             }
         }
 
@@ -43,6 +49,36 @@ namespace Assets.Scripts.Spawn_collectable
                 if (!c.GetNetworkActiveState())
                     c.RpcChangeNetworkState(true);
             }
+        }
+
+        private IEnumerator firstCountdown()
+        {
+            countdownCounter.RpcChangeNetworkState("3");
+            yield return new WaitForSeconds(1);
+            countdownCounter.RpcChangeNetworkState("2");
+            yield return new WaitForSeconds(1);
+            countdownCounter.RpcChangeNetworkState("1");
+            yield return new WaitForSeconds(1);
+            countdownCounter.RpcChangeNetworkState("");
+            InitCollectables();  // Isn't on start! is it a problem?
+            yield return new WaitForSeconds(3);
+            StartCoroutine(startCountdown());
+        }
+
+        private IEnumerator startCountdown()
+        {
+            int i = Random.Range(minCountdown, maxCountdown + 1);
+            //wait countdown
+            while (i > 0)
+            {
+                countdownCounter.RpcChangeNetworkState("" + i);
+                i--;
+                yield return new WaitForSeconds(1);
+            }
+            countdownCounter.RpcChangeNetworkState("");
+            SpawnCoins();
+            yield return new WaitForSeconds(3);
+            StartCoroutine(startCountdown());
         }
     }
 }
---
+++ Assets/Scripts/Spawn collectable/CountDown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class CountDown : NetworkBehaviour
+{
+    Text myText;
+
+    private void Start()
+    {
+        myText = GetComponentInChildren<Text>();
+    }
+    
+    [ClientRpc]
+    public void RpcChangeNetworkState(string str)
+    {
+        myText.text = str;
+    }
+}
