ff6c0c4d8d7887186fc180d57b6530533c13915d
Andrea Jegher <andreaj-93@hotmail.it>
2017-05-30 17:26:26 +0200

    Added player number selector

---
+++ Assets/Scripts/UI/PlayerNumberSelector.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Networking;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class PlayerNumberSelector : MonoBehaviour {
+
+    public Text textField;
+    protected NetworkLobbyController lobbyController;
+    private Slider slider;
+    
+	void Start () {
+        lobbyController = (NetworkLobbyController)NetworkManager.singleton;
+        slider = GetComponent<Slider>();
+    }
+	
+	public void ChangeValue()
+    {
+        textField.text = ""+(int)slider.value;
+        lobbyController.minPlayers = (int)slider.value;
+    }
+}
