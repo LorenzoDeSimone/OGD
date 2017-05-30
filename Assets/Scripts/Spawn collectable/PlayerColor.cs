f5c38e18d2e51ac972fd7839a091e5d807315319
Andrea Jegher <andreaj-93@hotmail.it>
2017-05-30 18:52:38 +0200

    Changed color order

--- Assets/Scripts/Spawn collectable/PlayerColor.cs
+++ Assets/Scripts/Spawn collectable/PlayerColor.cs
@@ -7,8 +7,8 @@ public class PlayerColor
 {
     private static float RGBMultiplier = 0.004f;//Multiply [0,255] value with this to obtain value normalized in [0,1]
     private static Color[] color = { new Color(000                , 000                , 254 * RGBMultiplier),       //BLUE
-                                     new Color(000                , 254 * RGBMultiplier, 126 * RGBMultiplier),       //GREEN
                                      new Color(254 * RGBMultiplier, 000                , 000                ),       //RED
+                                     new Color(000                , 254 * RGBMultiplier, 126 * RGBMultiplier),       //GREEN
                                      new Color(255 * RGBMultiplier, 255 * RGBMultiplier, 001 * RGBMultiplier),       //YELLOW
                                    };
 
