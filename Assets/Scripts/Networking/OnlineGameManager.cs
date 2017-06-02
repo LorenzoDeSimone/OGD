67b32590b735d22b9caca615c6f59b800fb80275
Emanuele <emanuele.lattanzio@mail.polimi.it>
2017-06-02 21:45:10 +0200

    Timer "fixed"

--- Assets/Scripts/Networking/OnlineGameManager.cs
+++ Assets/Scripts/Networking/OnlineGameManager.cs
@@ -16,14 +16,15 @@ namespace Assets.Scripts.Networking
 
         void Start()
         {
-            GameObject go = Instantiate(timer);
-            NetworkServer.Spawn(go);
-            go.GetComponent<TimeManager>().RpcSetEndTime(Time.time + matchTime);
+            timer = Instantiate(timer);
+            NetworkServer.Spawn(timer);
             StartCoroutine(StartMatchCountDown());
         }
 
         private IEnumerator StartMatchCountDown()
         {
+            yield return new WaitForSecondsRealtime(1);
+            timer.GetComponent<TimeManager>().RpcSetEndTime(matchTime);
             yield return new WaitForSecondsRealtime(matchTime);
             EndMatch();
         }
