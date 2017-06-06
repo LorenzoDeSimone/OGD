60ea0df90398f4e81abd4282a8b45eed3d63dc72
Andrea Jegher <andreaj-93@hotmail.it>
2017-06-06 11:45:10 +0200

    Minor fix to lan connect logic again

--- Assets/Scripts/Networking/LanPlayMenu.cs
+++ Assets/Scripts/Networking/LanPlayMenu.cs
@@ -35,8 +35,7 @@ namespace Assets.Scripts.Networking
             do
             {
                 Debug.LogWarning("Searching");
-                if (networkExplorer.running)
-                    networkExplorer.StopBroadcast();
+                SafeStopBroadcast();
                 yield return new WaitForSeconds(0.1f);
                 networkExplorer.Initialize();
                 yield return new WaitForSeconds(0.1f);
@@ -55,7 +54,20 @@ namespace Assets.Scripts.Networking
                         IEnumerator bss = networkExplorer.broadcastsReceived.Values.GetEnumerator();
                         bss.MoveNext();
                         lobbyController.networkAddress = ((NetworkBroadcastResult)bss.Current).serverAddress;
-                        lobbyController.StartClient();
+                        yield return new WaitForSeconds(0.1f);
+
+                        SafeStopBroadcast();
+
+                        try
+                        {
+                            lobbyController.StartClient();
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.LogWarning(e.Message);
+                            continue;
+                        }
+
                         startdAsClient = true;
                     }
                     yield return new WaitForSeconds(waitTime);
@@ -63,9 +75,9 @@ namespace Assets.Scripts.Networking
 
                 if (!startdAsClient)
                 {
-                    if (networkExplorer.running)
-                        networkExplorer.StopBroadcast();
+                    SafeStopBroadcast();
                     yield return new WaitForSeconds(0.1f);
+
                     try
                     {
                         lobbyController.StartHost();
@@ -89,6 +101,12 @@ namespace Assets.Scripts.Networking
                 }
             }
             while (!startdAsClient && !startedAsServer);
+        }
+
+        private void SafeStopBroadcast()
+        {
+            if (networkExplorer.running)
+                networkExplorer.StopBroadcast();
         }
 
         protected override void TryInitMenu()
