98c2d8ad93c30b49bb27ba194aa559b947119444
Andrea Jegher <andreaj-93@hotmail.it>
2017-06-16 17:41:06 +0200

    Minor fix

--- Assets/Scripts/Networking/LanPlayMenu.cs
+++ Assets/Scripts/Networking/LanPlayMenu.cs
@@ -17,7 +17,7 @@ namespace Assets.Scripts.Networking
         {
             networkExplorer = lobbyController.GetNetExplorer();
 
-#if UNITY_STANDALONE_WINDOWS
+#if UNITY_STANDALONE_WIN
             isHostingMatch = true;
 #endif
 
