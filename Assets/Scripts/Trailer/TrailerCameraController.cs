18e98d6ba06d34f34206704793d8fe9f933ec1af
Emanuele <emanuele.lattanzio@mail.polimi.it>
2017-06-17 15:53:17 +0200

    TrailerScene

---
+++ Assets/Scripts/Trailer/TrailerCameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerCameraController : MonoBehaviour {
+
+    public float speed = 5;
+    public float zoomSpeed = 5;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.L))
+        {
+            transform.position = transform.position + Vector3.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            transform.position = transform.position + Vector3.left * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.I))
+        {
+            transform.position = transform.position + Vector3.up * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            transform.position = transform.position + Vector3.down * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.M))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1 * Time.deltaTime * speed);
+        }
+        else if (Input.GetKey(KeyCode.N))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1 * Time.deltaTime * speed);
+        }
+    }
+}
---
+++ Assets/Scripts/Trailer/TrailerEnemySpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerEnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public float speed;
+    private bool spawned, first;
+    private float finalY;
+
+    void Start()
+    {
+        first = false;
+        spawned = false;
+        finalY = enemy.transform.position.y + 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawned)
+        {
+            enemy.transform.position = enemy.transform.position + Vector3.up * Time.deltaTime * speed;
+            if (enemy.transform.position.y > finalY)
+                spawned = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<Assets.Scripts.Player.PlayerDataHolder>() && !first)
+        {
+            spawned = first = true;
+        }
+    }
+}
