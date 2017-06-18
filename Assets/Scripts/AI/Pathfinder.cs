122afb7e83f419290295482b6db3e3e8e40d6672
Lorenzo De Simone <lorenz.desimone@gmail.com>
2017-06-18 16:54:15 +0200

    Pathfinding module isolated in a script

---
+++ Assets/Scripts/AI/Pathfinder.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    private AStarStepSolver currentAStarStepSolver;
+    private GameObject planetToReach;
+    private bool isPathfindingCoroutineRunning;
+    private Radar myRadar;
+
+    void Start()
+    {
+        isPathfindingCoroutineRunning = false;
+        myRadar = GetComponentInChildren<Radar>();
+        planetToReach = myRadar.GetMyGround().collider.gameObject;
+    }
+
+    public GameObject GetPlanetToReach()
+    {
+        return planetToReach;
+    }
+
+    public bool IsPathfindingStillCoroutineRunning()
+    {
+        return isPathfindingCoroutineRunning;
+    }
+
+    public void FindPath(Node start, Node end)
+    {
+        if(!isPathfindingCoroutineRunning)
+        {
+            currentAStarStepSolver = new AStarStepSolver(start, end);
+            isPathfindingCoroutineRunning = true;
+            StartCoroutine(CalculatePath());
+        }
+    }
+
+    public IEnumerator CalculatePath()
+    {
+        while (currentAStarStepSolver.Step())
+        {
+            yield return new WaitForSeconds(0f);
+        }
+
+        Edge[] path = currentAStarStepSolver.solution;
+        planetToReach = path[path.Length - 1].to.sceneObject;
+        isPathfindingCoroutineRunning = false;
+    }
+}
--- Assets/Scripts/AI/Patroller Bot/PatrollerBot.cs
+++ Assets/Scripts/AI/Patroller Bot/PatrollerBot.cs
@@ -4,29 +4,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-
 public class PatrollerBot : NetworkBehaviour
 {
     public float standStillTime = 3f;
 
     private Movable myMovable;
     private Radar myRadar;
+    private Pathfinder myPathfinder;
 
     private Movable.CharacterInput input;
-    private AStarStepSolver currentAStarStepSolver;
 
-    private GameObject planetToReach;
-    private bool isPathfindingCoroutineRunning;
     private bool playerHit;
 
     void Start()
     {
         myMovable = GetComponent<Movable>();
         myRadar = GetComponentInChildren<Radar>();
+        myPathfinder = GetComponent<Pathfinder>();
+
         input = new Movable.CharacterInput();
         RandomizeDirection();
-        planetToReach = myRadar.GetMyGround().collider.gameObject;
-        isPathfindingCoroutineRunning = false;
         playerHit = false;
     }
 
@@ -62,9 +59,9 @@ public class PatrollerBot : NetworkBehaviour
             return;
 
         //The agent reached its target planet
-        if(GameObject.ReferenceEquals(planetToReach, myRadar.GetMyGround().collider.gameObject))
+        if(GameObject.ReferenceEquals(myPathfinder.GetPlanetToReach(), myRadar.GetMyGround().collider.gameObject))
         {
-            if(!isPathfindingCoroutineRunning)
+            if(!myPathfinder.IsPathfindingStillCoroutineRunning())
             {
                 Graph graph = PathFindingManager.GetGraph();
                 Radar targetPlayerRadar = PlayerDataHolder.GetLocalPlayer().GetComponentInChildren<Radar>();
@@ -72,10 +69,8 @@ public class PatrollerBot : NetworkBehaviour
                 Node end = GetNodeFromGameObject(targetPlayerRadar.GetMyGround().collider.gameObject);
                 if (!GameObject.ReferenceEquals(start.sceneObject, end.sceneObject))
                 {
-                    currentAStarStepSolver = new AStarStepSolver(start, end);
-                    isPathfindingCoroutineRunning = true;
                     RandomizeDirection();
-                    StartCoroutine(CalculatePath());
+                    myPathfinder.FindPath(start, end);
                 }
             }
         }
@@ -85,10 +80,8 @@ public class PatrollerBot : NetworkBehaviour
                                                                 PathFindingManager.maxDistanceAdjacencies,
                                                                 LayerMask.GetMask("Walkable"));
 
-            //Debug.DrawRay(transform.position, transform.up * PathFindingManager.maxDistanceAdjacencies, Color.green);
-
             if (planetToReachCheck.collider != null &&
-                        GameObject.ReferenceEquals(planetToReach, planetToReachCheck.collider.gameObject))
+                        GameObject.ReferenceEquals(myPathfinder.GetPlanetToReach(), planetToReachCheck.collider.gameObject))
                 input.jump = true;
         }
 
@@ -99,51 +92,6 @@ public class PatrollerBot : NetworkBehaviour
     {
         foreach (Edge currEdge in path)
             Debug.DrawLine(currEdge.from.sceneObject.transform.position, currEdge.to.sceneObject.transform.position, Color.red);
-    }
-
-    /*private void ColorAll(List<Node> nodeList, Color color)
-    {
-        foreach (Node n in nodeList)
-            n.sceneObject.GetComponent<SpriteRenderer>().color = color;
-    }*/
-
-    private IEnumerator CalculatePath()
-    {
-
-        //foreach (Node n in currentAStarStepSolver.unvisited)
-        //    n.sceneObject.GetComponent<SpriteRenderer>().color = Color.magenta;
-        //foreach (Node n in currentAStarStepSolver.visited)
-        //    n.sceneObject.GetComponent<SpriteRenderer>().color = Color.red;
-        while (currentAStarStepSolver.Step())
-        {
-            //ColorAll(currentAStarStepSolver.visited, Color.red);
-            //currentAStarStepSolver.current.sceneObject.GetComponent<SpriteRenderer>().color = Color.green;
-
-            //foreach (Node n in currentAStarStepSolver.unvisited)
-            //    n.sceneObject.GetComponent<SpriteRenderer>().color = Color.magenta;
-            /*
-            foreach (Node n in currentAStarStepSolver.visited)
-                Debug.LogWarning("VISITED: " + n.sceneObject.name);
-            */
-
-            //Debug.LogError("CCCC");
-            //OutlineSet(currentAStarStepSolver.visited, Color.red);
-            //OutlineNode(AStarStepSolver.current, Color.green);
-            yield return new WaitForSeconds(0f);
-        }
-
-        Edge[] path = currentAStarStepSolver.solution;
-
-        // check if there is a solution
-        /*if (path.Length == 0)
-            Debug.LogError("No solution");
-        else
-        {
-            DrawPath(path);
-            Debug.LogError("Path!");
-        }*/
-        planetToReach = path[path.Length-1].to.sceneObject;
-        isPathfindingCoroutineRunning = false;
     }
 
     private bool CanMove()
