7cd3869d26339703b6b0f6ba0efc794d472d98e6
Andrea Jegher <andreaj-93@hotmail.it>
2017-06-20 17:59:35 +0200

    Intro scene added and minor fixes

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string sceneName="Lobby";
+    public float loadTime = 2.0f;
+
+	private void Start ()
+    {
+        StartCoroutine(OpenSceneAfterTime());
+	}
+
+    private IEnumerator OpenSceneAfterTime()
+    {
+        yield return new WaitForSeconds(loadTime);
+        SceneManager.LoadScene(sceneName);
+    }
+}
