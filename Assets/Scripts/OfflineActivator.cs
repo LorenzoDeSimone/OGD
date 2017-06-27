75ad5a7771edec9e39810708b863acb0e8ce9877
Andrea Jegher <andreaj-93@hotmail.it>
2017-06-27 11:44:27 +0200

    Minor fix

---
+++ Assets/Scripts/OfflineActivator.cs
@@ -0,0 +1,11 @@
+﻿using Assets.Scripts.Networking;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OfflineActivator : MonoBehaviour
+{
+    void Update ()
+    {
+        GetComponent<Text>().enabled = !NetworkLobbyController.instance.Online;
+	}
+}
