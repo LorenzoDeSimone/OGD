2a085e2d6c90f0f93ba1fbb0bf0462f14a5b0d71
Andrea Jegher <andreaj-93@hotmail.it>
2017-06-27 11:39:24 +0200

    Added offile load message

---
+++ Assets/Scripts/OnlineDeactivator.cs
@@ -0,0 +1,13 @@
+﻿using Assets.Scripts.Networking;
+using UnityEngine;
+
+public class OnlineDeactivator : MonoBehaviour
+{
+    void Update ()
+    {
+	    if(NetworkLobbyController.instance.Online)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+}
